8aa04b7608c9636dfe72dd946b347459a5bb2a9f
designatedcoder <83379213+designatedcoder@users.noreply.github.com>
2025-05-11 21:19:04 -0400

    set up questions, current question, and random answers

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,8 +1,34 @@
 <script setup lang="ts">
+    import { computed, onMounted, ref } from 'vue';
     import { Head, Link } from '@inertiajs/vue3';
     import Card from '@/components/ui/card/Card.vue'
     import CardHeader from '@/components/ui/card/CardHeader.vue';
     import CardTitle from '@/components/ui/card/CardTitle.vue';
+
+    const questions = ref([])
+    const currentQuestionIndex = ref(0)
+    const loading = ref(false)
+
+    onMounted(() => {
+        getQuestions()
+    })
+
+    const currentQuestion = computed(() => {
+        return questions.value[currentQuestionIndex.value]
+    });
+
+    const randomAnswers = computed(() => {
+       return [currentQuestion.value.correctAnswer,
+            ...currentQuestion.value.incorrectAnswers].sort((a,b)=> a < b ? -1 : 1)
+    });
+
+    const getQuestions = async () => {
+        loading.value = true
+        const resp = await axios.get('https://the-trivia-api.com/v2/questions?limit=10')
+        questions.value = resp.data
+        loading.value = false
+        console.log(resp.data)
+    };
 </script>
 
 <template>
@@ -37,7 +63,7 @@
             </nav>
         </header>
 
-        <Card class="my-auto p-6 w-3/4">
+        <Card class="my-auto p-6 w-3/4" v-if="currentQuestion">
             <CardHeader>
                 <CardTitle class="text-center">
                     <div class="flex justify-center text-xl space-x-5 mb-6">
@@ -70,7 +96,7 @@
                     </div>
 
                     <p class="text-xl mt-4">
-                        Lorem ipsum dolor sit amet?
+                        {{ currentQuestion.question.text }}
                     </p>
                 </CardTitle>
             </CardHeader>
@@ -78,25 +104,20 @@
             <div class="flex flex-col justify-center space-y-6 mx-auto w-full md:w-1/2">
                 <button type="button"
                     class="rounded-md font-medium transition-all bg-primary text-primary-foreground shadow-xs hover:bg-primary/90 h-auto px-4 py-2 cursor-pointer"
+                    v-for="(answer, index) in randomAnswers"
+                    :key="index"
                 >
-                    Lorem, ipsum dolor.
-                </button>
-                <button type="button"
-                    class="rounded-md font-medium transition-all bg-primary text-primary-foreground shadow-xs hover:bg-primary/90 h-auto px-4 py-2 cursor-pointer"
-                >
-                    Lorem, ipsum dolor.
-                </button>
-                <button type="button"
-                    class="rounded-md font-medium transition-all bg-primary text-primary-foreground shadow-xs hover:bg-primary/90 h-auto px-4 py-2 cursor-pointer"
-                >
-                    Lorem, ipsum dolor.
-                </button>
-                <button type="button"
-                    class="rounded-md font-medium transition-all bg-primary text-primary-foreground shadow-xs hover:bg-primary/90 h-auto px-4 py-2 cursor-pointer"
-                >
-                    Lorem, ipsum dolor.
+                    {{ answer }}
                 </button>
             </div>
+        </Card>
+
+        <Card class="my-auto p-6 w-3/4" v-else>
+            <CardHeader>
+                <CardTitle class="text-center">
+                    <p>...loading</p>
+                </CardTitle>
+            </CardHeader>
         </Card>
     </div>
 </template>
