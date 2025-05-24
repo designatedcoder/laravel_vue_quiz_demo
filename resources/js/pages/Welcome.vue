663c8ce85389093f013bc03b6e778cf15f5557a4
designatedcoder <83379213+designatedcoder@users.noreply.github.com>
2025-05-24 18:07:57 -0400

    full play functionality

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -4,10 +4,15 @@
     import Card from '@/components/ui/card/Card.vue'
     import CardHeader from '@/components/ui/card/CardHeader.vue';
     import CardTitle from '@/components/ui/card/CardTitle.vue';
+    import CardContent from '@/components/ui/card/CardContent.vue';
+    import Button from '@/components/ui/button/Button.vue';
 
     const questions = ref([])
     const currentQuestionIndex = ref(0)
     const loading = ref(false)
+    const correctScore = ref(0)
+    const incorrectScore = ref(0)
+    const completed = ref(false)
 
     onMounted(() => {
         getQuestions()
@@ -17,17 +22,47 @@
         return questions.value[currentQuestionIndex.value]
     });
 
+    const remainingCount = computed(() => {
+        return questions.value.length - currentQuestionIndex.value
+    });
+
     const randomAnswers = computed(() => {
        return [currentQuestion.value.correctAnswer,
             ...currentQuestion.value.incorrectAnswers].sort((a,b)=> a < b ? -1 : 1)
     });
 
+    const finalScore = computed(() => {
+        return correctScore.value * 10
+    });
+
+    const hasCompleted = computed(() => {
+        return completed.value = true ? remainingCount.value <= 0 : remainingCount.value >= 0
+    });
+
     const getQuestions = async () => {
         loading.value = true
-        const resp = await axios.get('https://the-trivia-api.com/v2/questions?limit=10')
+        const resp = await axios.get('https://the-trivia-api.com/v2/questions?limit=10&difficulties=medium')
         questions.value = resp.data
         loading.value = false
         console.log(resp.data)
+    };
+
+    const guess = (answer) => {
+        if (answer === currentQuestion.value.correctAnswer) {
+            correctScore.value++
+        } else {
+            incorrectScore.value++
+        }
+        currentQuestionIndex.value++
+    };
+
+    const resetQuiz = () => {
+        completed.value = false
+        questions.value = []
+        correctScore.value = 0
+        incorrectScore.value = 0
+        currentQuestionIndex.value = 0
+        getQuestions()
     };
 </script>
 
@@ -36,7 +71,9 @@
         <link rel="preconnect" href="https://rsms.me/" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
     </Head>
-    <div class="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] dark:bg-[#0a0a0a] lg:justify-center lg:p-8">
+    <div class="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] dark:bg-[#0a0a0a] lg:justify-center lg:p-8"
+        :class="{ 'bg-[url(/storage/images/celebration-19390_256.gif)] bg-no-repeat bg-cover' : hasCompleted }"
+    >
         <header class="not-has-[nav]:hidden mb-6 w-full max-w-[335px] text-sm lg:max-w-4xl">
             <nav class="flex items-center justify-end gap-4">
                 <Link
@@ -72,25 +109,25 @@
                             <p>00:00</p>
                         </div>
                     </div>
-                    <div class="flex justify-center text-xl space-x-10">
+                    <div class="flex flex-col items-center text-xl space-y-5 lg:flex-row lg:justify-center lg:space-y-0 lg:space-x-10">
                         <div class="flex flex-col items-end w-1/4">
-                            <div class="flex justify-between w-full">
+                            <div class="flex justify-between space-x-5 w-full">
                                 <p>Total:</p>
-                                <p>10</p>
+                                <p>{{ questions.length }}</p>
                             </div>
-                            <div class="flex justify-between w-full">
+                            <div class="flex justify-between space-x-5 w-full">
                                 <p>Remaining:</p>
-                                <p>7</p>
+                                <p>{{ remainingCount }}</p>
                             </div>
                         </div>
                         <div class="flex flex-col items-end w-1/4">
-                            <div class="flex justify-between text-green-700 w-full">
+                            <div class="flex justify-between space-x-5 text-green-700 w-full">
                                 <p>Correct:</p>
-                                <p>10</p>
+                                <p>{{ correctScore }}</p>
                             </div>
-                            <div class="flex justify-between text-red-600 w-full">
+                            <div class="flex justify-between space-x-5 text-red-600 w-full">
                                 <p>Incorrect:</p>
-                                <p>7</p>
+                                <p>{{ incorrectScore }}</p>
                             </div>
                         </div>
                     </div>
@@ -106,18 +143,36 @@
                     class="rounded-md font-medium transition-all bg-primary text-primary-foreground shadow-xs hover:bg-primary/90 h-auto px-4 py-2 cursor-pointer"
                     v-for="(answer, index) in randomAnswers"
                     :key="index"
+                    @click.prevent="guess(answer)"
                 >
                     {{ answer }}
                 </button>
             </div>
         </Card>
 
-        <Card class="my-auto p-6 w-3/4" v-else>
+        <Card class="my-auto p-6 w-3/4" v-else-if="loading">
             <CardHeader>
                 <CardTitle class="text-center">
                     <p>...loading</p>
                 </CardTitle>
             </CardHeader>
+        </Card>
+
+        <Card class="my-auto p-6 w-3/4" v-else>
+            <CardHeader>
+                <CardTitle class="text-center text-xl">
+                    <p>Congrats, here is your final score!</p>
+                    <p>{{ finalScore }}%</p>
+                </CardTitle>
+            </CardHeader>
+            <CardContent class="text-center">
+                <Button
+                    class="cursor-pointer"
+                    @click.prevent="resetQuiz"
+                >
+                        Play Again!
+                </Button>
+            </CardContent>
         </Card>
     </div>
 </template>
